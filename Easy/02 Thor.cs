ccb9e0b4c7fcd451452b3aef0d2cefe7a4cddd8f
GyuPark <gyutie@gmail.com>
2020-06-18 20:58:57 +0900

    Create 02 Thor.cs
    미해결

---
+++ Easy/02 Thor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * ---
+ * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+ 조금 욕심내서 최단거리로 이동하게 만들겠다!!
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int lightX = int.Parse(inputs[0]); // the X position of the light of power
+        int lightY = int.Parse(inputs[1]); // the Y position of the light of power
+        int initialTx = int.Parse(inputs[2]); // Thor's starting X position
+        int initialTy = int.Parse(inputs[3]); // Thor's starting Y position
+
+        // game loop
+        while (true)
+        {
+            int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns Thor can move. Do not remove this line.
+
+            //움직여야 할 방향
+            var dir = "";
+
+            //8방 중 한 방향 정하기
+
+
+            // A single line providing the move to be made: N NE E SE S SW W or NW
+            Console.WriteLine(dir);
+        }
+    }
+}
