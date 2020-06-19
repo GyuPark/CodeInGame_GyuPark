ccb9ba9913ef211c2c1bdf804075332c4fc891de
GyuPark <gyutie@gmail.com>
2020-06-19 10:53:17 +0900

    Update 02 Power of Thor 1.cs

--- Easy/02 Power of Thor 1.cs
+++ Easy/02 Power of Thor 1.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
  * the standard input according to the problem statement.
  * ---
  * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
- 조금 욕심내서 최단거리로 이동하게 만들겠다!!
  **/
 class Player
 {
@@ -30,10 +29,29 @@ class Player
             //움직여야 할 방향
             var dir = "";
 
-            //삼각형을 그려서 8방 중 한 방향 정하기
-            // - h = a + b라면? 직선이동 ( 8방중 하나)
-            // - h > a + b라면? h = a + b가 될 때까지 이동하기
+            //삼각형(h, a, b)을 그려서 8방 중 한 방향 정하기
+            var displacementX = lightX - initialTx; //b
+            var displacementY = lightY - initialTy; //a
 
+            var angle = 0d;
+
+            if (displacementX == 0)
+            {
+                if (displacementY < 0)
+                {
+
+                }
+                else if (displacementY > 0)
+                {
+
+                }
+            }
+            else
+            {
+                angle = Math.Atan(displacementY / displacementX); //theta
+            }
+
+            Console.Error.WriteLine("Debug " + angle);
 
             // A single line providing the move to be made: N NE E SE S SW W or NW
             Console.WriteLine(dir);
