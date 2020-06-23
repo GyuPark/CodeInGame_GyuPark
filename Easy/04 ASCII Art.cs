6c99241a2739f0a25fc28db4b877d8956cc30e9f
GyuPark <gyutie@gmail.com>
2020-06-23 10:11:50 +0900

    ASCII ARTS

---
+++ Easy/04 ASCII Art.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int L = int.Parse(Console.ReadLine());
+        int H = int.Parse(Console.ReadLine());
+        string T = Console.ReadLine();
+
+        for (int i = 0; i < H; i++)
+        {
+            string ROW = Console.ReadLine();
+        }
+
+        //ROW를 알파벳 별로 나누어 array에 담는다.
+        string[] alphabets = new string[26];
+
+        Console.WriteLine("answer");
+    }
+}
