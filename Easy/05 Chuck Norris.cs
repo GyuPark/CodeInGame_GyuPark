84dac2cfb8bfbb509f8200bb46f251595468d386
GyuPark <gyutie@gmail.com>
2020-06-25 18:01:52 +0900

    Stack & Chuck Norris

--- Easy/05 Chuck Norris.cs
+++ Easy/05 Chuck Norris.cs
@@ -19,9 +19,34 @@ class Solution
         string MESSAGE = Console.ReadLine();
         Console.Error.WriteLine(MESSAGE);
 
-        //어떤 자료구조가 좋을까?
-        //십진법 key 32 ~ 126
-        //Symbol value
+        var startingNumber = 32; //필요한 ASCII Code 시작 숫자
+        var endingNumber = 127; //필요한 ASCII Code 끝 숫자
+        var arraySize = endingNumber - startingNumber + 1;
+
+        //어떤 자료구조가 좋을까? 물론 Dictionary... key = symbol : value = binary in string
+        Dictionary<string, string> symbolForBinary = new Dictionary<string, string>();
+
+        //value ( binary ) 저장할 컬랙션
+        string[] values_in_binary = new string[arraySize]; //배열에 32부터 127까지 이진법으로 표현한 숫자를 문자열 형식으로 담기.
+        for (int i = 0; i < values_in_binary.Length; i++)
+        {
+            //keys_in_binary[i] = decimal 에서 binary로 변환한 걸 string으로 저장
+        }
+
+        //key ( symbol ) 저장할 컬랙션
+        var symbols = "";
+        for (int i = startingNumber; i <= endingNumber; i++) //ASCII 문자를 인코딩해서 넣어주기
+        {
+            symbols += (char)i; //인코딩
+            //Console.Error.WriteLine(symbols);
+        }
+        string[] symbols_in_string = symbols.Split();
+
+        //Dictionary key-value 설정하기
+        for (int i = 0; i < arraySize; i++)
+        {
+            symbolForBinary.Add(symbols_in_string[i], values_in_binary[i]);
+        }
 
         Console.WriteLine("answer");
     }
---
+++ Stack 구현.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CStack
+{
+    #region Field
+    private int p_index; //top item에 접근을 용이하게 만드는 index
+    List<int> list; //ArrayList로 Stack ArrayList는 참고로 더이상 사용하는 걸 권장하지 않는다. List<T>써라.
+
+    /// <summary>
+    /// The size of the stack
+    /// </summary>
+    public int Count
+    {
+        get { return list.Count; }
+    } 
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// a constructor
+    /// </summary>
+    public CStack()
+    {
+        list = new List<int>();
+        p_index = -1; //why?
+    }
+
+    /// <summary>
+    /// Push method
+    /// </summary>
+    /// <param name="item">the item to push to the stack</param>
+    public void Push(object item)
+    {
+        list.Add(item);
+        p_index++;//?
+    }
+
+    /// <summary>
+    /// Pop method
+    /// </summary>
+    /// <returns>returns the object to pop</returns>
+    public object Pop()
+    {
+        object obj = list[p_index];
+        list.RemoveAt(p_index);
+        p_index--;
+        return obj;
+    }
+
+    /// <summary>
+    /// method that clears the entire stack
+    /// </summary>
+    public void Clear()
+    {
+        list.Clear();
+        p_index = -1;
+    }
+
+    /// <summary>
+    /// Method that returns the top item in the stack
+    /// </summary>
+    /// <returns>the top item in the stack</returns>
+    public object Peek()
+    {
+        return list[p_index];
+    } 
+    #endregion
+}
