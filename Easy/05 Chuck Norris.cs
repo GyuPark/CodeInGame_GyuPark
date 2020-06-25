8e97f084ded5141e2b0201a32990b7917b3342ee
GyuPark <gyutie@gmail.com>
2020-06-25 10:59:00 +0900

    알고리즘 책 업로드

--- Easy/05 Chuck Norris.cs
+++ Easy/05 Chuck Norris.cs
@@ -9,14 +9,19 @@ using System.Collections.Generic;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+//https://www.ascii-code.com/ 아스키 문자 리스트
+
 class Solution
 {
     static void Main(string[] args)
     {
         string MESSAGE = Console.ReadLine();
+        Console.Error.WriteLine(MESSAGE);
 
-        // Write an answer using Console.WriteLine()
-        // To debug: Console.Error.WriteLine("Debug messages...");
+        //어떤 자료구조가 좋을까?
+        //십진법 key 32 ~ 126
+        //Symbol value
 
         Console.WriteLine("answer");
     }
